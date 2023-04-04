79c9a2407a6a117c24c4a5cb6ba7f5fde68833d0
Lucas <lucas_emanuel_sc@hotmail.com>
2023-04-04 19:35:08 -0300

    fix: req 7

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -5,15 +5,21 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
+def read_mode(path):
+    if ".csv" in path:
+        content = CsvImporter.import_data(path)
+    elif ".json" in path:
+        content = JsonImporter.import_data(path)
+    elif ".xml" in path:
+        content = XmlImporter.import_data(path)
+    return content
+
+
 class Inventory:
     @staticmethod
     def import_data(path, type):
-        if ".csv" in path:
-            content = CsvImporter.import_data(path)
-        elif ".json" in path:
-            content = JsonImporter.import_data(path)
-        elif ".xml" in path:
-            content = XmlImporter.import_data(path)
+        content = read_mode(path)
         if type == "simples":
             return SimpleReport.generate(content)
-        return CompleteReport.generate(content)
+        elif type == "completo":
+            return CompleteReport.generate(content)
