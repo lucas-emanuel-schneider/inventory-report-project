af92e70f9891218874944a00a30502a6384dc79e
Lucas <lucas_emanuel_sc@hotmail.com>
2023-04-05 15:11:54 -0300

    feat: main and fix: the main issues(complexity)

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,33 +1,14 @@
-# from inventory_report.importer.xml_importer import XmlImporter
-# from inventory_report.importer.csv_importer import CsvImporter
-# from inventory_report.importer.json_importer import JsonImporter
-# from inventory_report.inventory.inventory_refactor import InventoryRefactor
-# from inventory_report.reports.simple_report import SimpleReport
-# from inventory_report.reports.complete_report import CompleteReport
-
-# import sys
+from inventory_report.inventory.inventory import Inventory
+import sys
 
 
-# def get_report_type(type, result):
-#     if type == "simples":
-#         sys.stdout.write(SimpleReport.generate(result))
-#     else:
-#         sys.stdout.write(CompleteReport.generate(result))
-
-
-# def main():
-#     if len(sys.argv) < 3:
-#         return sys.stderr.write("Verifique os argumentos\n")
-#     path = sys.argv[1]
-#     report_type_method = sys.argv[2]
-#     if path.endswith(".csv"):
-#         importer = CsvImporter()
-#     elif path.endswith(".json"):
-#         importer = JsonImporter()
-#     elif path.endswith(".xml"):
-#         importer = XmlImporter()
-#     else:
-#         return sys.stderr.write("Arquivo não suportado!\n")
-#     data = InventoryRefactor(importer)
-#     result = data.import_data(path, report_type_method)
-#     get_report_type(report_type_method, result)
+def main():
+    if len(sys.argv) < 3:
+        return sys.stderr.write("Verifique os argumentos\n")
+    try:
+        path = sys.argv[1]
+        report_type_method = sys.argv[2]
+        report = Inventory.import_data(path, report_type_method)
+        sys.stdout.write(report)
+    except (ValueError):
+        sys.stdout.write("Verifique seus argumentos\n")
